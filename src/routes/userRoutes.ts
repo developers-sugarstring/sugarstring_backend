10aa6a9c68f988856444253c9773da59b4b63847
developers-sugarstring <developers@sugarstrings.ai>
2025-10-27 23:47:44 +0530

    superadmin access

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -16,18 +16,18 @@ import { DiseaseZodSchema } from "../schemas/IDiseaseSchema";
 const router = Router();
 
 // CREATE
-router.post("/create-user", validate(UserZodSchema), verifyJWT, allowOnly("OperationsUser"), createUser);
+router.post("/create-user", validate(UserZodSchema), verifyJWT, allowOnly("OperationsUser", "SuperAdmin"), createUser);
 // READ ALL
 router.get("/get-users", getUsers);
 // READ SINGLE
 router.get("/get-user/:id", getUserById);
 // UPDATE
-router.put("/update-user/:id", validate(UserZodSchema), verifyJWT, allowOnly("OperationsUser", "ReportingUser"), updateUser);
+router.put("/update-user/:id", validate(UserZodSchema), verifyJWT, allowOnly("OperationsUser", "ReportingUser", "SuperAdmin"), updateUser);
 // DELETE
 router.delete("/delete-user/:id", deleteUser);
 
 //DISEASE ROUTE
-router.put("/update-user-disease/:id", validate(DiseaseZodSchema), verifyJWT, allowOnly("ReportingUser"),  updateUserDisease);
+router.put("/update-user-disease/:id", validate(DiseaseZodSchema), verifyJWT, allowOnly("ReportingUser","SuperAdmin"),  updateUserDisease);
 
 // USER BY SAMPLE ID 
 router.get("/get-user-by-sample/:sampleId", getUserBySampleId);
